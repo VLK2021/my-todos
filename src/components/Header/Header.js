583f41b6d0f51abd02ae6b98811b9949aa7d1b5f
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-19 21:21:33 +0300

    did change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,21 @@ import Form from "../Form/Form";
 const Header = () => {
     const [formAddVisible, setFormAddVisible] = useState(false);
 
+    const onchange = () => {
+        document.body.classList.toggle("lightTherm")
+    }
+
 
     return (
         <div className={'Header'}>
             <div className={'Header-text'}>
                 my todos
+            </div>
+
+            <div>
+                <input type="checkbox"
+                       onChange={event => onchange(event.target.value)}
+                />
             </div>
 
             <div className={'Header-form'}>
