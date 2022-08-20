78e37b96c25fbb33b09426998b4f304bd30d234e
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-20 20:51:57 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import './FooterStyle.css';
 const Footer = () => {
     return (
         <div className={'Footer'}>
+            <div className={'footer-text'}>React Redux 2022</div>
 
         </div>
     );
