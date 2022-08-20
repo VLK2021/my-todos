3ff402aa57b6716e0facabb2c9869cb6d3da9cbb
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-20 21:16:37 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './FooterStyle.css';
+import logo512 from './logo512.png';
+
 
 const Footer = () => {
     return (
         <div className={'Footer'}>
-            <div className={'footer-text'}>React Redux 2022</div>
-
+            <div className={'Footer-logo'}>
+                <img src={logo512} alt="logo"/>
+                <div className={'footer-text'}>React Redux 2022</div>
+            </div>
+            <h5>Copyright © 2022 Volodymyr Kostiuk.</h5>
         </div>
     );
 };
